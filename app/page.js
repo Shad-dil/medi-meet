c88fdc65b8ed1b4df32aa141a510a0f84cc9890d
Dilshad <helloshad3@gmail.com>
2025-07-10 17:31:30 +0530

    designing landing page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { features } from "@/lib/data";
-import { ArrowRight } from "lucide-react";
+import { creditBenefits, features, testimonials } from "@/lib/data";
+import { ArrowRight, Check, Stethoscope } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -93,6 +93,134 @@ export default function Home() {
               );
             })}
           </div>
+        </div>
+      </section>
+
+      {/* pricing */}
+
+      <section className="py-20">
+        <div className="container mx-auto px-4">
+          <div className="text-center mb-16">
+            <h2 className="text-center text-3xl md:text-4xl font-bold mb-4 text-white ">
+              Consulting Packages
+            </h2>
+            <p className="text-muted-foreground text-lg max-w-2xl mx-auto">
+              Choose the perfect consultation package that fits your healthcare
+            </p>
+          </div>
+          <div>
+            {/* pricing table  */}
+            <Card className="bg-muted/20 mt-12 border-emerald-900/30">
+              <CardHeader>
+                <CardTitle className="text-xl font-semibold flex text-white items-center">
+                  {" "}
+                  <Stethoscope className="w-5 h-5 mr-2 text-emerald-400" /> How
+                  our credit system works{" "}
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <ul className="space-y-3">
+                  {creditBenefits.map((benifit, i) => (
+                    <li key={i} className="flex items-start">
+                      <div className="mr-3 mt-1 bg-emerald-900/20 p-1 rounded-full">
+                        <Check className="h-4 w-4 text-emerald-400" />
+                      </div>
+                      <p
+                        className="text-muted-foreground"
+                        dangerouslySetInnerHTML={{ __html: benifit }}
+                      />
+                    </li>
+                  ))}
+                </ul>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </section>
+
+      <section className="py-20 bg-muted/30">
+        <div className="container mx-auto px-4">
+          <div className="text-center mb-16">
+            <Badge
+              variant="outline"
+              className="bg-emerald-900/30 border-emerald-700/30 text-emerald-400 text-sm font-medium px-4 py-2"
+            >
+              Success Stories
+            </Badge>
+            <h2 className="text-center text-3xl md:text-4xl font-bold mb-4 text-white ">
+              What our Users say
+            </h2>
+            <p className="text-muted-foreground text-lg max-w-2xl mx-auto">
+              Hear from patients and doctors who use our platform
+            </p>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {testimonials.map((testimonial, i) => {
+              return (
+                <Card
+                  key={i}
+                  className="border-emerald-900/20 transition-all duration-300 hover:border-emerald-800/20"
+                >
+                  <CardContent>
+                    <div className="flex items-center mb-4">
+                      <div className="w-12 h-12 rounded-full bg-emerald-900/20  flex items-center justify-center mr-4">
+                        <span className="text-emerald-400 font-bold">
+                          {testimonial.initials}
+                        </span>
+                      </div>
+                      <div>
+                        <h4 className="font-semibold text-white">
+                          {testimonial.name}
+                        </h4>
+                        <p className="text-muted-foreground text-sm">
+                          {testimonial.role}
+                        </p>
+                      </div>
+                    </div>
+                    <p className="text-muted-foreground">
+                      &quot;{testimonial.quote}&quot;
+                    </p>
+                  </CardContent>
+                </Card>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+
+      <section className="py-20 bg-muted/30">
+        <div className="container mx-auto px-4">
+          <Card className="bg-gradient-to-r from-emerald-900/30 to-emerald-950/20 border-emerald-800/20">
+            <CardContent className="pt-6">
+              <div>
+                <h2 className="text-3xl md:text-4xl font-bold text-white mb-6">
+                  Ready to take control of your healthcare
+                </h2>
+                <p className="text-lg text-muted-foreground mb-8">
+                  join thousands of users who have simplified their healthcare
+                  journey with our platform. Get started today and experience
+                  healthcare the way it should be
+                </p>
+                <div className="flex flex-col sm:flex-row gap-4">
+                  <Button
+                    asChild
+                    size="lg"
+                    className="bg-emerald-600 text-white hover:bg-emerald-700"
+                  >
+                    <Link href="/sign-up">Sign Up Now</Link>
+                  </Button>
+                  <Button
+                    asChild
+                    size="lg"
+                    variant="outline"
+                    className="bg-emerald-700/30 hover:bg-muted/80"
+                  >
+                    <Link href="/pricing">View Pricing</Link>
+                  </Button>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
         </div>
       </section>
     </div>
